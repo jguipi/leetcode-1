0f8a1008cc822c7666def0556c5da877146f61cb
jguipi <juste_g@hotmail.com>
2021-08-25 14:57:52 -0400

    Co-authored-by: dmahmalat, jguipi, bdelagesap, wong-kenneth-sap and parth

--- javascript/014_Longest_Common_Prefix.js
+++ javascript/014_Longest_Common_Prefix.js
@@ -2,6 +2,28 @@
  * @param {string[]} strs
  * @return {string}
  */
- var longestCommonPrefix = function(strs) {
-    
+var longestCommonPrefix = function (strs) {
+  let activeWord = strs[0];
+  if (strs.length === 1) {
+    return strs[0];
+  } else {
+    activeWord = strs[0];
+    //keep track of word
+    for (let i = 1; i < strs.length; i++) {
+      if (activeWord.length > strs[i].length) {
+        activeWord = activeWord.substr(0, strs[i].length);
+      }
+      //keep track of letter
+      for (let j = 0; j < strs[i].length; j++) {
+        if (activeWord.substr(0, j + 1) !== strs[i].substr(0, j + 1)) {
+          activeWord = activeWord.substr(0, j);
+          break;
+        }
+      }
+    }
+  }
+
+  return activeWord;
 };
+
+console.log(longestCommonPrefix(["aaa", "aa", "aaa"]));
